dfed58c66805c525860d746abec987e490994684
nubsym <45231307+nubsym@users.noreply.github.com>
2023-10-15 15:47:32 +0800

    Add files via upload

---
+++ tracking/analysis_results.py
@@ -0,0 +1,30 @@
+import _init_paths
+import matplotlib.pyplot as plt
+plt.rcParams['figure.figsize'] = [8, 8]
+
+from lib.test.analysis.plot_results import plot_results, print_results, print_per_sequence_results
+from lib.test.evaluation import get_dataset, trackerlist
+
+trackers = []
+dataset_name = 'otb'  # lasot_extension_subset  tnl2k
+"""sttrack"""
+trackers.extend(trackerlist(name='sttrack', parameter_name='baseline', dataset_name=dataset_name,
+                            run_ids=None, display_name='STTRACK'))
+"""TransT"""
+# trackers.extend(trackerlist(name='TransT_N2', parameter_name=None, dataset_name=None,
+#                             run_ids=None, display_name='TransT_N2', result_only=True))
+# trackers.extend(trackerlist(name='TransT_N4', parameter_name=None, dataset_name=None,
+#                             run_ids=None, display_name='TransT_N4', result_only=True))
+"""pytracking"""
+# trackers.extend(trackerlist('atom', 'default', None, range(0,5), 'ATOM'))
+# trackers.extend(trackerlist('dimp', 'dimp18', None, range(0,5), 'DiMP18'))
+# trackers.extend(trackerlist('dimp', 'dimp50', None, range(0,5), 'DiMP50'))
+# trackers.extend(trackerlist('dimp', 'prdimp18', None, range(0,5), 'PrDiMP18'))
+# trackers.extend(trackerlist('dimp', 'prdimp50', None, range(0,5), 'PrDiMP50'))
+
+dataset = get_dataset(dataset_name)
+# dataset = get_dataset('otb', 'nfs', 'uav', 'tc128ce')
+# plot_results(trackers, dataset, dataset_name, merge_results=True, plot_types=('success', 'norm_prec'),
+#              skip_missing_seq=False, force_evaluation=True, plot_bin_gap=0.05)
+print_results(trackers, dataset, dataset_name, merge_results=True, plot_types=('success', 'prec','norm_prec'))
+# print_results(trackers, dataset, 'UNO', merge_results=True, plot_types=('success', 'prec'))
